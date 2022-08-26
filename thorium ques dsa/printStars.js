a61938d54731fcde70eaf34f97b61a3384b29d65
gautam kumar <gautam01kr11@gmail.com>
2022-08-26 09:06:58 +0530

    added

--- thorium ques dsa/printStars.js
+++ thorium ques dsa/printStars.js
@@ -1,18 +1,86 @@
-// 1.print stars(
-//     *
-//     **
-//     *
-//     **
-//     ***)
+// for(let i=0; i<=5; i++){
+//     process.stdout.write(`${i}`);
+// }
 
-// Decode a string recursively encoded as count followed by substring
+// *
+// **
+// ***
+// ****
+// *****
 
-// Input: "2[ab]c3[xyz]" 
-// Output: "ababcxyzxyzxyz" 
+for(let i=1; i<=5; i++){   // row
+    for(j=1; j<=i; j++){     //column
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+}
 
-// Input: "3[b2[ca]]" 
-// Output: "bcacabcacabcaca" 
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
 
+for(let i=1; i<=5; i++){   // row
+    for(j=1; j<=i; j++){     //column
+        process.stdout.write(`${i} `);
+    }
+    process.stdout.write("\n");
+}
 
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
 
+for(let i=1; i<=5; i++){   // row
+    for(j=1; j<=i; j++){     //column
+        process.stdout.write(`${j} `);
+    }
+    process.stdout.write("\n");
+}
+
+console.log("<<<<<<<<<<<<<<<<<<<<<<first>>>>>>>>>>>>>>>>>>>>>>")
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+for(let i=5; i>=1; i--){   // row
+    for(j=1; j<=i; j++){     //column
+        process.stdout.write(`${j} `);
+    }
+    process.stdout.write("\n");
+}
+
+//      *
+//     * *
+//    * * *
+//   * * * *
+
+for(let i=1; i<=5; i++){   // row
+    for(k=1; k<=(5-i); k++){
+        process.stdout.write(" "); 
+    }
+    for(j=1; j<=i; j++){     //column
+        process.stdout.write("*" + " ");
+    }
+    process.stdout.write("\n");
+}
+
+//      *
+//     * *
+//    * * *
+//   * * * *
+
+for(let i=5; i>=1; i--){   // row
+    for(k=1; k<=(5-i); k++){
+        process.stdout.write(" "); 
+    }
+    for(j=1; j<=i; j++){     //column
+        process.stdout.write("*" + " ");
+    }
+    process.stdout.write("\n");
+}
 
